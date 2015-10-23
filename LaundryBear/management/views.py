81f5eb932ec9f9d95d9ecac882ee26a8b03b3226
Eli Tan <eliedrian1996@gmail.com>
2015-10-23 18:09:47 +0800

    add temporary views

--- LaundryBear/management/views.py
+++ LaundryBear/management/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
 
-# Create your views here.
+from database.models import LaundryShop
+
+
+class LaundryMenuView(TemplateView):
+    template_name = 'management/shop/laundrybearmenu.html'
+
+
+class LaundryCreateView(CreateView):
+    template_name = 'management/shop/addlaundryshop.html'
+    form_class = None
+    model = LaundryShop
+    fields = ['name', 'province', 'city', 'barangay', 'street', 'building', 'contact_number', 'email', 'website', 'hours_open', 'days_open']
+
