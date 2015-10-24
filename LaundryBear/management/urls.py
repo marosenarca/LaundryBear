a2d7654537cdedca104349f3c326c54dcf1794b0
Eli Tan <eliedrian1996@gmail.com>
2015-10-24 21:25:20 +0800

    make html work with django

--- LaundryBear/management/urls.py
+++ LaundryBear/management/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.LaundryMenuView.as_view(), name='menu'),
-    #url(r'^shops/add$', views.LaundryCreateView.as_view(), name='add-shop'),
-    url(r'^shops/edit/(?P<pk>\d+)$', views.LaundryUpdateView.as_view(), name='edit-shop')
+    url(r'^shops/add$', views.LaundryCreateView.as_view(), name='add-shop'),
+    url(r'^shops/edit/(?P<pk>\d+)$', views.LaundryUpdateView.as_view(), name='edit-shop'),
+    url(r'^shops/services/add$', views.ServiceCreateView.as_view(), name='add-service')
 ]
--- LaundryBear/management/views.py
+++ LaundryBear/management/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import UpdateView, TemplateView
+from django.views.generic import CreateView, ListView, UpdateView, TemplateView
 
-from database.models import LaundryShop
+from database.models import LaundryShop, Service
 
 
 class LaundryMenuView(TemplateView):
@@ -11,3 +11,18 @@ class LaundryMenuView(TemplateView):
 class LaundryUpdateView(UpdateView):
     template_name = 'management/shop/editlaundryshop.html'
     model = LaundryShop
+    fields = ['name',]
+
+
+class LaundryCreateView(CreateView):
+    template_name = 'management/shop/addlaundryshop.html'
+    model = LaundryShop
+    fields = ['barangay', 'building', 'city', 'contact_number',
+        'days_open', 'email', 'hours_open', 'name', 'province',
+        'street', 'website']
+
+
+class ServiceCreateView(CreateView):
+    template_name = 'management/shop/addservice.html'
+    model = Service
+    fields = ['name', 'description']
