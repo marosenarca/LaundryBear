61b25c48949b697d1ccfbe369daebbc8ebe401dc
Eli Tan <eliedrian1996@gmail.com>
2015-10-25 01:20:03 +0800

    add urls for laundry listing

--- LaundryBear/management/urls.py
+++ LaundryBear/management/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.LaundryMenuView.as_view(), name='menu'),
     url(r'^shops/add$', views.LaundryCreateView.as_view(), name='add-shop'),
-    url(r'^shops/edit/(?P<pk>\d+)$', views.LaundryUpdateView.as_view(), name='edit-shop')
+    url(r'^shops/edit/(?P<pk>\d+)$', views.LaundryUpdateView.as_view(),
+        name='edit-shop'),
+    url(r'^shops/list$', views.LaundryListView.as_view(), name='list-shops')
 ]
