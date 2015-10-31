82423344de5c757635ccc06c66485906138dd6c6
Eli Edrian Tan <eliedrian1996@gmail.com>
2015-11-01 03:28:17 +0800

    load up hours and days fields, load up services
    lacks deleting a service

--- LaundryBear/management/static/management/js/app.js
+++ LaundryBear/management/static/management/js/app.js
@@ -49,41 +49,8 @@ $("#set-service-button").click(function() {
 
 	var $tableBody = $("#service-table-body");
 	$tableBody.append(compiledTemplate);
-	$tableBody.find("a.edit-service-button").last().click(function() {
-		var $tr = $(this).closest("tr");
-		var pk = $tr.data("service-pk");
-		var optionName = $tr.data("service-name");
-		var price = $tr.data("service-price");
-		var description = $tr.data("service-description");
-		// remove from table
-		$tr.remove();
-
-		// add back into combobox and set as selected
-		var optionTemplate = $("#option-template").html();
-		var compiledOptionTemplate = optionTemplate.replace(/__value__/, pk).replace(/__optionname__/g, optionName).replace(/__description__/, description);
-		var $option = $(compiledOptionTemplate);
-		$("#service-input").append($option);
-		$("#service-input").val(pk);
-
-		// put price back
-		$("#price-input").val(price);
-		return false;
-	});
-	$tableBody.find("a.delete-service-button").last().click(function() {
-		var $tr = $(this).closest("tr");
-		var pk = $tr.data("service-pk");
-		var optionName = $tr.data("service-name");
-		var description = $tr.data("service-description");
-		// remove from table
-		$tr.remove();
-		// add back into combobox
-		var optionTemplate = $("#option-template").html();
-		var compiledOptionTemplate = optionTemplate.replace(/__value__/, pk).replace(/__optionname__/g, optionName).replace(/__description__/, description);
-		var $option = $(compiledOptionTemplate);
-		$("#service-input").val("null");
-		$("#service-input").append($option);
-		return false;
-	});
+	$tableBody.find("a.edit-service-button").last().click(editServiceHandler);
+	$tableBody.find("a.delete-service-button").last().click(deleteServiceHandler);
 
 	// pop item off
 	$selectedService.remove();
@@ -141,5 +108,78 @@ $("#days-container input").on("change", function() {
 	var dayString = checked.join(', ');
 	$("#id_days_open").val(dayString);
 });
+
+// load hours_open and days_open and services
+{
+	var hours_open = $("#id_hours_open").val();
+	var hours_open_array = hours_open.split(" - ");
+	$("#open-time").val(hours_open_array[0]);
+	$("#close-time").val(hours_open_array[1]);
+
+	var days_open = $("#id_days_open").val();
+	var days_open_array = days_open.split(", ");
+	$.each(days_open_array, function(index, day) {
+		$("#" + day.toLowerCase()).prop("checked", true);
+	});
+
+	var $services = $("#price-formset-container").children();
+	$.each($services, function(index, service) {
+		var $service = $(service);
+		var itemIndex = $service.data("item");
+		var servicePk = $service.find("#id_price_set-" + itemIndex + "-service").val();
+		if (servicePk.length === 0 ) {
+			return;
+		}
+		var price = $service.find("#id_price_set-" + itemIndex + "-price").val();
+		var $option = $("#service-input").find("option[value=" + servicePk + "]");
+		var description = $option.data("service-description");
+		var serviceName = $option.data("service-name");
+		$option.remove();
+		var template = $("#table-row-template").html();
+		var compiledTemplate = template.replace(/__service-name__/g, serviceName).replace(/__description__/g, description).replace(/__price__/g, price).replace(/__pk__/g, servicePk);
+
+		var $tableBody = $("#service-table-body");
+		$tableBody.append(compiledTemplate);
+		$tableBody.find("a.edit-service-button").last().click(editServiceHandler);
+		$tableBody.find("a.delete-service-button").last().click(deleteServiceHandler);
+	});
+}
+
+function editServiceHandler() {
+	var $tr = $(this).closest("tr");
+	var pk = $tr.data("service-pk");
+	var optionName = $tr.data("service-name");
+	var price = $tr.data("service-price");
+	var description = $tr.data("service-description");
+	// remove from table
+	$tr.remove();
+
+	// add back into combobox and set as selected
+	var optionTemplate = $("#option-template").html();
+	var compiledOptionTemplate = optionTemplate.replace(/__value__/, pk).replace(/__optionname__/g, optionName).replace(/__description__/, description);
+	var $option = $(compiledOptionTemplate);
+	$("#service-input").append($option);
+	$("#service-input").val(pk);
+
+	// put price back
+	$("#price-input").val(price);
+	return false;
+}
+
+function deleteServiceHandler() {
+	var $tr = $(this).closest("tr");
+	var pk = $tr.data("service-pk");
+	var optionName = $tr.data("service-name");
+	var description = $tr.data("service-description");
+	// remove from table
+	$tr.remove();
+	// add back into combobox
+	var optionTemplate = $("#option-template").html();
+	var compiledOptionTemplate = optionTemplate.replace(/__value__/, pk).replace(/__optionname__/g, optionName).replace(/__description__/, description);
+	var $option = $(compiledOptionTemplate);
+	$("#service-input").val("null");
+	$("#service-input").append($option);
+	return false;
+}
 
 $('#logout_submit').click(function(){$('#logout_form').submit();});
