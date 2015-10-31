7cdf55aa6c017a1c053b9eea63fb0c8bd25eaa6f
marosenarca <marose96@outlook.com>
2015-10-31 20:01:56 +0800

    with add new service ui design

--- LaundryBear/management/static/management/js/app.js
+++ LaundryBear/management/static/management/js/app.js
@@ -58,6 +58,9 @@ $("#days-container input").on("change", function() {
 
 $('#logout_submit').click(function(){$('#logout_form').submit();});
 
+// $('#closeModal').click(function(){
+// 	$('form')[0].reset();
+// 	});
 
 $("#closeModal").on("click",function(){
 	$('form')[0].reset();
