93db14ee08be57ecf56b5b3603a5976b2bd18a55
Eli Edrian Tan <eliedrian1996@gmail.com>
2015-10-31 12:33:39 +0800

    write some tests regarding login

--- LaundryBear/management/tests.py
+++ LaundryBear/management/tests.py
@@ -116,3 +116,51 @@ class ContextDataTestCase(TestCase):
             barangay__icontains='lsbarangay')
         self.assertEqual(list(expected_shop_list),
             list(response.context['shop_list']))
+
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_superuser(username='test', password='runner',
+            email='test@runner.com')
+
+    def test_login_required_redirect(self):
+        response = self.client.get(reverse('management:menu'), follow=True)
+        expected = 'management/account/login.html'
+        self.assertEqual(response.templates[0].name, expected)
+
+    def test_redirect_to_menu_after_login_with_next(self):
+        response = self.client.get(reverse('management:menu'), follow=True)
+        request = response.request
+        response = self.client.post('{0}?{1}'.format(request['PATH_INFO'],
+            request['QUERY_STRING']),
+            {'username': 'test', 'password': 'runner'}, secure=True,
+            follow=True)
+        expected = 'management/shop/laundrybearmenu.html'
+        self.assertEqual(response.templates[0].name, expected)
+
+    def test_invalid_login(self):
+        response = self.client.get(reverse('management:menu'), follow=True)
+        request = response.request
+        response = self.client.post('{0}?{1}'.format(request['PATH_INFO'],
+            request['QUERY_STRING']),
+            {'username': 'tester', 'password': 'runner'}, secure=True,
+            follow=True)
+        expected = 'management/shop/laundrybearmenu.html'
+        self.assertNotEqual(response.templates[0].name, expected)
+
+    def test_redirect_to_menu_after_login_without_next(self):
+        response = self.client.post(reverse('management:login-admin'),
+            {'username': 'test', 'password': 'runner'}, secure=True,
+            follow=True)
+        expected = 'management/shop/laundrybearmenu.html'
+        self.assertEqual(response.templates[0].name, expected)
+
+    def test_only_superuser(self):
+        # create non-superuser account
+        User.objects.create_user(username='weak', password='soweak')
+        response = self.client.post(reverse('management:login-admin'),
+            {'username': 'test', 'password': 'runner'}, secure=True,
+            follow=True)
+        expected = 'management/shop/laundrybearmenu.html'
+        self.assertNotEqual(response.templates[0].name, expected)
+
