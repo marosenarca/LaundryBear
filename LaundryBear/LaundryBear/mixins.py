cd79047885e67c4e646fb28df0036fd86ef1e0ef
Eli Tan <eliedrian1996@gmail.com>
2015-11-10 21:02:19 +0800

    try to improve login redirects

--- LaundryBear/LaundryBear/mixins.py
+++ LaundryBear/LaundryBear/mixins.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
-from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect
@@ -13,7 +12,9 @@ class LoginRequiredMixin(object):
     """
     View mixin which requires that the user is authenticated.
     """
-    @method_decorator(user_passes_test(lambda u:u.is_staff, login_url='/client/login'))
+    @login_required(login_url='/client/login')
     def dispatch(self, request, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(
             request, *args, **kwargs)
+
+
