d0fa4c18e62ad5b5ff27e70bb01d7843b714ecd0
Miriam Ty <mariethegreat89303@gmail.com>
2015-12-12 15:02:20 +0000

    take stuff from deploy branch take stuff from deploy branch

--- LaundryBear/LaundryBear/urls.py
+++ LaundryBear/LaundryBear/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
@@ -25,7 +26,6 @@ import management
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^management/', include('management.urls', namespace='management')),
-    url(r'^client/', include('client.urls', namespace='client'))
+    url(r'^', include('client.urls', namespace='client'))
 ]
 
-urlpatterns += staticfiles_urlpatterns()
